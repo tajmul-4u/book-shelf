2b7fa5ca6e04b46d6250dae76ed42406d0ee722a
Tajmul Islam <tajmulislam137@gmail.com>
2025-10-04 22:24:40 +0600

    React router setup and the project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-amber-200 text-2xl text-black">Book shelf connected</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
